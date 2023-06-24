864f37fe7797b3648bf24fd24ac97d5b5d04efb8
Chetana <chetanzambare26@gmail.com>
2023-06-24 12:30:34 +0530

    Initial project creation

---
+++ ConnectionProject/src/main/java/com/connection/ConnectionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionProjectApplication.class, args);
+	}
+
+}
